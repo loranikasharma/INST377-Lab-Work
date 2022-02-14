f2e32b569009f1a104c3898333f1ffd32af42171
loranikasharma <sharma_loranika@yahoo.com>
2022-02-14 08:54:32 -0500

    Tetrmino added

--- tetris/script.js
+++ tetris/script.js
@@ -5,9 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10
     //The Tetrominoes
-    const LTetromino = [[1, width + 1, width * 2 +1, 2],
+    const lTetromino = [[1, width + 1, width * 2 +1, 2],
     [width, width+1, width+2, width*2+2], [1, width+1, width*2+1, width*2],
     [width, width*2, width*2+1, width*2+2]]
+    const tTetromino=[[1, width, width+1, width+2], [1, width+1,width+2,width*2+1]
+    [width, width+1, width+2, width*2+1], [1, width, width+1, width*2+1]]
+    const oTetromino=[[0, 1, width, width+1], [0, 1, width, width+1], [0, 1, width, width+1
+    ],[0, 1, width, width+1]]
+    const iTetromino=[[1, width+1, width*2+1, width*3+1], [width, width+1, width+2, width+3]
+    [1, width+1, width*2+1, width*3+1], [width, width+1, width+2, width+3]]
 
     
 
