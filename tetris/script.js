be625d778985f037ca2fbd888d59156a18d9478e
loranikasharma <sharma_loranika@yahoo.com>
2022-02-14 15:16:34 -0500

    scoreDisplay

--- tetris/script.js
+++ tetris/script.js
@@ -129,6 +129,23 @@ function rotate(){
             displayShape()
         }
     })
+    function addScore(){
+        for(let i = 0; i<199;i+=width){
+            const row = [i,i+1,i+2,i+3,i+4,i+5,i+6,i+7,i+8,i+9]
+            if(row.every(index=> squares[index].contains('taken'))){
+                score+=10
+                ScoreDisplay.innerHTML=score
+                row.forEach(index=>{
+                    squares[index].classList.remove('taken')
+                })
+                const squaresRemoved=squares.splice(i,width)
+                squares=squaresRemoved.concat(squares)
+                squares.forEach(cell=>grid.appendChild(cell))
+
+            }
+
+        }
+    }
 
 
 });
