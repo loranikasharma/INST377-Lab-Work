9996651be613a7a88dbabeea903ce88e6b7227f4
loranikasharma <sharma_loranika@yahoo.com>
2022-02-16 09:10:12 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -66,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function addScore() {
     for (let i = 0; i < 199; i += width) {
       const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9];
-      if (row.every((index) => squares[index].contains('taken'))) {
+      if (row.every((index) => squares[index].classList.contains('taken'))) {
         score += 10;
         ScoreDisplay.innerHTML = score;
         row.forEach((index) => {
