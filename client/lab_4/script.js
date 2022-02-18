a6549c49abed21dba0156863f3532e3accd05c04
loranikasharma <sharma_loranika@yahoo.com>
2022-02-17 21:23:16 -0500

    commit

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,17 +2,30 @@
 let slidePosition = 0;
 const slides = document.querySelectorAll('carousel_item');
 const totalSlides = slides.length;
-function moveToNextSlide() {
-  if (slidePosition === totalSlides - 1) {
-    slidePosition = 0;
+function updateSlidePosition() {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const slide of slides) {
+    slide.classList.remove('.carousel_item--visible');
+    slide.classList.add('.carousel_item--hidden');
   }
+  // eslint-disable-next-line no-unused-expressions
+  slides[slidePosition].classList.add('.carousel_item--visible');
+}
+function moveToNextSlide() {
+  if (slidePosition === totalSlides) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
 }
 function moveToPrevSlide() {
-  if (slidePosition === totalSlides) {
+  if (slidePosition === 0) {
     slidePosition = 0;
   } else {
     slidePosition--;
   }
+  updateSlidePosition();
 }
 document.querySelector('carousel_button--next').addEventListener('click', () => {
   moveToNextSlide();
@@ -20,4 +33,3 @@ document.querySelector('carousel_button--next').addEventListener('click', () => 
 document.querySelector('carousel_button--prev').addEventListener('click', () => {
   moveToPrevSlide();
 });
-
