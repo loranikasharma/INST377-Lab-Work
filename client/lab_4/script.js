e6c82f0111d7b2239766a7ea9b99b25a74c94730
loranikasharma <sharma_loranika@yahoo.com>
2022-02-17 21:07:11 -0500

    commit

---
+++ client/lab_4/script.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-plusplus */
+let slidePosition = 0;
+const slides = document.querySelectorAll('carousel_item');
+const totalSlides = slides.length;
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  }
+}
+function moveToPrevSlide() {
+  if (slidePosition === totalSlides) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+}
+document.querySelector('carousel_button--next').addEventListener('click', () => {
+  moveToNextSlide();
+});
+document.querySelector('carousel_button--prev').addEventListener('click', () => {
+  moveToPrevSlide();
+});
+
