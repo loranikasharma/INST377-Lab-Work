f0b19531060d062582fc937a1658de71094deee7
loranikasharma <sharma_loranika@yahoo.com>
2022-02-18 14:34:34 -0500

    commit-completed

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,18 +1,25 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-plusplus */
+
 let slidePosition = 0;
 const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
+document.querySelector('.Next').addEventListener('click', () => {
+  moveToNextSlide();
+});
+document.querySelector('.Previous').addEventListener('click', () => {
+  moveToPrevSlide();
+});
 function updateSlidePosition() {
-  // eslint-disable-next-line no-restricted-syntax
   for (const slide of slides) {
-    slide.classList.remove('.carousel_item--visible');
-    slide.classList.add('.carousel_item--hidden');
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
   }
-  // eslint-disable-next-line no-unused-expressions
-  slides[slidePosition].classList.add('.carousel_item--visible');
+
+  slides[slidePosition].classList.add('carousel_item--visible');
 }
 function moveToNextSlide() {
-  if (slidePosition === totalSlides) {
+  if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition++;
@@ -21,15 +28,9 @@ function moveToNextSlide() {
 }
 function moveToPrevSlide() {
   if (slidePosition === 0) {
-    slidePosition = totalSlides-1;
+    slidePosition = totalSlides - 1;
   } else {
     slidePosition--;
   }
   updateSlidePosition();
 }
-document.querySelector('.test2').addEventListener('click', () => {
-  moveToNextSlide();
-});
-document.querySelector('.test1').addEventListener('click', () => {
-  moveToPrevSlide();
-});
