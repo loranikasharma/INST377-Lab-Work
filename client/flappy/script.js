a7a70799816becabb26804a58812a347080dab2f
loranikasharma <sharma_loranika@yahoo.com>
2022-03-30 14:44:24 -0400

    commit

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -1,17 +1,23 @@
+/* eslint-disable prefer-const */
+/* eslint-disable prefer-template */
 document.addEventListener('DOMContentLoaded', () => {
   const bird = document.querySelector('.bird');
   const gameDisplay = document.querySelector('.game-container');
   const ground = document.querySelector('.ground');
 
-  let birdLeft = 200;
+  let birdLeft = 220;
   let birdBottom = 100;
   let gravity = 2;
 
   function startGame() {
     birdBottom -= gravity;
-    bird.style.bottom = birdBottom + 'px'
+    bird.style.bottom = birdBottom + 'px';
     bird.style.left = birdLeft + 'px';
   }
   let timerId = setInterval(startGame, 20);
-  clearInterval(timerId);
+  function jump() {
+    birdBottom += 50;
+    bird.style.bottom = birdBottom + 'px';
+  }
+  document.addEventListener('keyup', jump);
 });
