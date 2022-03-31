26b4f309090a4658119a40394cfe5772edce618b
loranikasharma <sharma_loranika@yahoo.com>
2022-03-31 11:09:54 -0400

    commit

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -15,9 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
     bird.style.left = birdLeft + 'px';
   }
   let timerId = setInterval(startGame, 20);
+  function control(e) {
+    if (e.keyCode === 32) {
+      // eslint-disable-next-line no-use-before-define
+      jump();
+    }
+  }
   function jump() {
+    if (birdBottom < 500)birdBottom += 50;
     birdBottom += 50;
     bird.style.bottom = birdBottom + 'px';
   }
-  document.addEventListener('keyup', jump);
+  document.addEventListener('keyup', control);
+
+  function generateObstacle() {
+    const obstacle = document.createElement('.div');
+    obstacle.classList.add('obstacle');
+  }
 });
